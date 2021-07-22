bb8ba1ffe82058d2ae7e7dce00096cb814d6f2ae
Neill Clift <neillclift@live.com>
2021-07-21 17:07:47 -0700

    Make it crash

--- crashy.cpp
+++ crashy.cpp
@@ -1,9 +1,13 @@
-#define USE_BOOST
+#define __STRICT_ANSI__
+#define BOOST_HAS_INT128
+#define BOOST_MP_HAS_IMMINTRIN_H
 
-#include <iostream>
 #include <boost/multiprecision/cpp_int.hpp>
+
+typedef boost::multiprecision::number<boost::multiprecision::cpp_int_backend<256, 256, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void> > NTYPE;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	NTYPE sv = 267386880;
+	sv >>= 1;
 }
